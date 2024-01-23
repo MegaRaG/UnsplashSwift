d7c4d7737c003d826f16f0ff3ea1e80ba6e64b8f
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-23 13:34:30 +0100

    Initial Commit

---
+++ UnsplashSwift/UnsplashSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashSwiftApp.swift
+//  UnsplashSwift
+//
+//  Created by Ahmed OMEROVIC on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
